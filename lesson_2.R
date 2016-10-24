fcb1550f0074336849d7ba4f8162d51af847e1bb
AlexRego <alexandre.rego@aggiemail.usu.edu>
2016-10-24 10:10:00 -0600

    lesson_2.R

--- lesson_2.R
+++ lesson_2.R
@@ -1,1 +1,32 @@
-#test
+## Exercises
+
+# 1
+for(i in 20:10){
+  print(i)
+}
+
+# 2
+for(i in 20:10){
+  if(i%%2 == 0){
+    print(i)
+  }
+}
+
+# 3
+is.prime <- function(x){
+  if(x<2){
+    return(FALSE)
+  } else{
+    if(x == 2){
+    return(TRUE)
+      } else{
+          for(d in (2:sqrt(x)+1)){
+            if(x%%d == 0){
+              return(FALSE)
+                } else{
+                  return(TRUE)
+        }
+      }
+    }
+  }
+}
