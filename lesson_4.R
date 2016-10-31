c89f4bc03736afbfa3a8ffc67cca4a89a8f85f7b
AlexRego <alexandre.rego@aggiemail.usu.edu>
2016-10-31 12:00:09 -0600

    lesson_4.R

--- lesson_4.R
+++ lesson_4.R
@@ -1,14 +1,14 @@
 #lesson 4
 # 1
-x <- replicate(10,(rnorm(n=1,mean=runif(1,0,10),sd=runif(1,0,10))))
+x <- replicate(10,(rnorm(n=10,mean=runif(1,0,10),sd=runif(1,0,10))))
 
-# 2
+# 2 - totes wrong
 pseudosum.num <- function(x){
   cat('Mean:', mean(x), '\n')
   cat('SD:', var(x)**2, '\n')
 }
 
-# 3
+# 3 - totes wrong
 pseudosum.cat <- function(x){
   if(!is.numeric(x)){
   cat('Mean:', mean(x), '\n')
@@ -18,7 +18,7 @@ pseudosum.cat <- function(x){
   }
 }
 
-# 4
+# 4 - totes wrong
 pseudosum <- function(x){
   if(is.numeric(x)){
     return(pseudosum.num(x))
@@ -27,16 +27,17 @@ pseudosum <- function(x){
   }
 }
 
-# 5
+# 5 - WIP
 dna <- 'ACGATATACGAG'
 translate <- function(x){
   ## split sequence into codons
   codons <- sapply(seq(1,nchar(x),3), function(i) substr(x,i,i+2))
   ##create fake codon lookup table
-  codontable <- matrix(c('thr','ile','tyr','glu'),ncol=4)
+  codonmatrix <- matrix(c('thr','ile','tyr','glu'),ncol=4)
   colnames(codontable) <- c('ACG','ATA','TAC','GAG')
+  codontable <- as.table(codonmatrix)
   ## pull from matrix based on codon
   for(i in codons){
-    
+    return(codontable$i)
   }
 }
