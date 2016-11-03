c624725e511aab8a1be8f933750a5862a848484d
AlexRego <alexandre.rego@aggiamil.usu.edu>
2016-11-02 20:36:46 -0600

    lesson_3.R

--- lesson_3.R
+++ lesson_3.R
@@ -1,6 +1,6 @@
 ## what even
 # 1
-new.cat <- function(cool, fat){
+new.cat <- function(cool, fat,...){
   output <- list(cool=cool, fat=fat)
   class(output) <- "cat"
   return(output)
@@ -23,20 +23,65 @@ race <- function(first, second){
 }
 
 # 2
-new.point <- function(x,y){
+new.point <- function(x,y,...){
   output <- list(x=x,y=y)
   class(output) <- 'point'
   return(output)
 }
 
-origin <- new.point(0,0)
-point1 <- new.point(6,9)
-
+first <- new.point(0,0)
+second <- new.point(6,9)
+third <- new.point(0,18)
 print.point <- function(point, ...){
   cat("The coordinates for this point are","(",point$x,",", point$y,")")
 }
 
-# 3 - WIP
-point.diff <- function(point1, point2){
-  if(!inherits(first,'point') | !inherits(second,'point'))
+# 3
+point.dist <- function(first, second){
+  if(!inherits(first,'point') | !inherits(second,'point')){
+    stop("Points not given")}
+  dist <- sqrt((first$x-second$x)**2 + (first$y-second$y)**2)
+  return(dist)
+}
+
+#4
+new.line <- function(first,second,...){
+  if(!inherits(first, 'point') | !inherits(second, 'point')) {
+    stop("Points not given")
+  }
+  output <- list(point1 = first,point2 = second)
+  # x1 <- first$x; x2 <- second$x; y1 <- first$y; y2 <- second$y
+  # plot(NA, xlim=c(0, 20), ylim=c(0, 20), xlab="x", ylab="y")
+  # segments(x1,y1,x2,y2)
+  # output <- recordPlot()
+  class(output) <- "line"
+  return(output)
+}
+line <- new.line(first,second)
+
+#5
+new.polygon <- function(first,second,third,...){
+  if(!inherits(first, 'point') | !inherits(second, 'point') | !inherits(third, 'point')){
+    stop("Points not given")
+  }
+  output <- list(point1 = first, point2 = second, point3 = third)
+  class(output) <- "polygon"
+  return(output)
+}
+polygon <- new.polygon(first,second,third)
+#6
+grapher <- function(input,...){
+  plot(NA, xlim=c(-20,20), ylim=c(-20, 20), xlab="x", ylab="y")
+  if(inherits(input, 'line')){
+      segments(x0 = input[1]$point1[1]$x, y0 = input[1]$point1[2]$y,
+        x1= input[2]$point2[1]$x, y1= input[2]$point2[2]$y)
+  }
+  if(inherits(input, 'polygon')){
+      segments(x0 = input[1]$point1[1]$x, y0 = input[1]$point1[2]$y,
+        x1= input[2]$point2[1]$x, y1= input[2]$point2[2]$y)
+      segments(x0 = input[2]$point2[1]$x, y0 = input[2]$point2[2]$y,
+        x1= input[3]$point3[1]$x, y1= input[3]$point3[2]$y)
+      segments(x0 = input[1]$point1[1]$x, y0 = input[1]$point1[2]$y,
+        x1= input[3]$point3[1]$x, y1= input[3]$point3[2]$y)
+    }
 }
